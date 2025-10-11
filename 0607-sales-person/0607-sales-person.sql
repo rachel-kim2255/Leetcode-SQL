30082d24c8a2598c3fd22d0999c990670b0abe9c
Rachel Kim <rachelkim2255@gmail.com>
2025-10-11 00:45:53 -0700

    Time: 1216 ms (88.25%), Space: 0B (100%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,12 +1,24 @@
-# Write your MySQL query statement below
+# KEY: NOT IN, LEFT JOIN
+# LEARN: 
 
-select  name
-FROM    SalesPerson
-where   name not in
-    (
-    SELECT  s.name
-    FROM    SalesPerson as s LEFT JOIN Orders as o 
-            ON s.sales_id = o.sales_id
-            LEFT JOIN Company as c 
-            ON  o.com_id = c.com_id
-    WHERE   c.name = 'RED')
+
+-- select  name
+-- FROM    SalesPerson
+-- where   name NOT IN
+--     (
+--     SELECT  s.name
+--     FROM    SalesPerson as s LEFT JOIN Orders as o 
+--             ON s.sales_id = o.sales_id
+--             LEFT JOIN Company as c 
+--             ON  o.com_id = c.com_id
+--     WHERE   c.name = 'RED')
+
+SELECT s.name
+FROM SalesPerson AS s
+WHERE NOT EXISTS (
+    SELECT 1
+    FROM Orders AS o
+    JOIN Company AS c ON o.com_id = c.com_id
+    WHERE o.sales_id = s.sales_id 
+      AND c.name = 'RED'
+);
