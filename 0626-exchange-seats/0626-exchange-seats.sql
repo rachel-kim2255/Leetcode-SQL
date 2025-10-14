e457e323632be7c0b1a31f950d1ff38d66be1321
Rachel Kim <rachelkim2255@gmail.com>
2025-10-13 23:35:13 -0700

    Time: 346 ms (42.82%), Space: 0B (100%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+# KEY : SUBQUEARY
+# LEARN: IN CASE, not possible id = id + 1
+
+
+SELECT  (CASE 
+        WHEN id != (SELECT count(*) FROM Seat) AND id % 2 = 1 THEN id + 1 
+        WHEN id != (SELECT count(*) FROM Seat) AND id % 2 = 0 THEN id - 1 
+        WHEN id = (SELECT count(*) FROM Seat) AND id % 2 = 0 THEN id - 1
+        ELSE id END) AS id, 
+        student
+FROM    Seat 
+ORDER BY id, student DESC;
+
+
