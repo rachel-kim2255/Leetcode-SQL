12c1fbf81e844a014ef49e601419d0439e8a8c4d
Rachel Kim <rachelkim2255@gmail.com>
2025-11-08 13:46:27 -0800

    Time: 440 ms (79.16%), Space: 0B (100%) - LeetHub

--- 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/1245-user-activity-for-the-past-30-days-i.sql
@@ -1,4 +1,7 @@
-# Write your MySQL query statement below
+# KEY: only for counting 30 days, INTERVAL 29 DAY
+# BETWEEN A AND B: including both A and B
+# BETWEEN ~ INTERVAL 30 DAY AND ~: 31 DAYS
+# INTERVAL 29 DAY: 30 DAYS 
 
 SELECT  activity_date AS day, count(distinct(user_id)) as active_users
 FROM    Activity
