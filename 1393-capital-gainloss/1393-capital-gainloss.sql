76cd1fe38b5ff81d313b5105263df73b45916e6b
Rachel Kim <rachelkim2255@gmail.com>
2025-12-28 16:27:48 -0800

    Time: 427 ms (98.67%), Space: 0B (100%) - LeetHub

--- 1393-capital-gainloss/1393-capital-gainloss.sql
+++ 1393-capital-gainloss/1393-capital-gainloss.sql
@@ -1,12 +1,16 @@
 # Write your MySQL query statement below
 
-WITH sales AS(
-    SELECT  stock_name, operation, SUM(price) as price
-    FROM    Stocks
-    GROUP BY stock_name, operation
-)
-SELECT  stock_name, SUM(IF(operation = 'Sell', price, -price)) AS capital_gain_loss
-FROM    sales
-GROUP BY stock_name
-ORDER BY stock_name asc;
+-- WITH sales AS(
+--     SELECT  stock_name, operation, SUM(price) as price
+--     FROM    Stocks
+--     GROUP BY stock_name, operation
+-- )
+-- SELECT  stock_name, SUM(IF(operation = 'Sell', price, -price)) AS capital_gain_loss
+-- FROM    sales
+-- GROUP BY stock_name;
 
+
+SELECT  stock_name,
+        SUM(CASE WHEN operation='Sell' THEN price ELSE -price END) AS capital_gain_loss
+FROM    Stocks
+GROUP BY stock_name
