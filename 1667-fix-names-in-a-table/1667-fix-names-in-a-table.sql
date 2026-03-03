55b818969314e256d7bf1a9c763859b5bf9c2066
Rachel Kim <rachelkim2255@gmail.com>
2026-03-02 17:37:45 -0800

    Time: 631 ms (59.54%), Space: 0B (100%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,6 +1,6 @@
 # users: pk(user_id)
 
 SELECT  user_id,
-        CONCAT(UPPER(LEFT(name, 1)), LOWER(SUBSTRING(name, 2, LENGTH(name)))) as name
+        CONCAT(UPPER(LEFT(name, 1)), LOWER(SUBSTRING(name, 2))) as name
 FROM    users
 ORDER BY user_id;
