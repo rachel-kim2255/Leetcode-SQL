c585fde115b6d50fcedeb84a0ae18aecbaaa318f
Rachel Kim <rachelkim2255@gmail.com>
2026-03-09 16:32:17 -0700

    Time: 510 ms (60.39%), Space: 0B (100%) - LeetHub

--- 1795-rearrange-products-table/1795-rearrange-products-table.sql
+++ 1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -1,9 +1,11 @@
-SELECT product_id, 'store1' AS store, store1 AS price
-FROM Products WHERE store1 IS NOT NULL
+SELECT  product_id, 'store1' as store, store1 as price
+FROM    products 
+WHERE   store1 is not null
 UNION ALL
-SELECT product_id, 'store2', store2
-FROM Products WHERE store2 IS NOT NULL
+SELECT  product_id, 'store2', store2 
+FROM    products
+WHERE   store2 is not null
 UNION ALL
-SELECT product_id, 'store3', store3
-FROM Products WHERE store3 IS NOT NULL
-ORDER BY product_id;
+SELECT  product_id, 'store3', store3
+FROM    Products 
+WHERE   store3 IS NOT NULL;
