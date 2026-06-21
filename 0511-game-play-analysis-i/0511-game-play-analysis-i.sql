f7e34a6e128f698d5eeff46de545c55af5a81ecc
Rachel Kim <rachelkim2255@gmail.com>
2026-06-21 14:02:57 -0700

    Time: 504 ms (56.7%), Space: 0B (100%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -1,6 +1,24 @@
-SELECT  player_id, MIN(event_Date) as first_login
-FROM    activity
-GROUP BY player_id
+-- SELECT  player_id, MIN(event_Date) as first_login
+-- FROM    activity
+-- GROUP BY player_id
+
+
+
+
+
+
+
+
+
+
+
+select  player_id, min(event_date) as first_login
+from    activity 
+group by player_id 
+
+
+
+
 
 
 
