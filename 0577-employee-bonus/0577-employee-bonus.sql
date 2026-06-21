318557c0c5c693ef74f9a72c550555916c28e904
Rachel Kim <rachelkim2255@gmail.com>
2026-06-21 14:05:58 -0700

    Time: 1426 ms (5.5%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,4 +1,22 @@
-SELECT  e.name, b.bonus
-FROM    employee e LEFT JOIN bonus b
+-- SELECT  e.name, b.bonus
+-- FROM    employee e LEFT JOIN bonus b
+--         on e.empId = b.empId
+-- WHERE   b.bonus < 1000 or b.bonus is null
+
+
+
+
+
+select  e.name as name, b.bonus
+from    employee as e LEFT JOIN bonus as b
         on e.empId = b.empId
-WHERE   b.bonus < 1000 or b.bonus is null
+where   b.bonus is null or b.bonus < 1000
+
+
+
+
+
+
+
+
+
