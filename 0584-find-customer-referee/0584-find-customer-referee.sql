20ccb203665c99d684b8f59348b475f3fc195a1c
Rachel Kim <rachelkim2255@gmail.com>
2026-06-23 16:46:31 -0700

    Time: 485 ms (70.48%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,5 +1,40 @@
-# Write your MySQL query statement below
+-- -- customer: id(pk)
+
+
+-- SELECT  name
+-- FROM    Customer
+-- WHERE   referee_id != 2 or referee_id is null
+
+
+
+
+
+
+
+
+
+
+-- -- # Write your MySQL query statement below
+
+-- -- SELECT  name
+-- -- FROM    Customer
+-- -- WHERE   referee_id != 2 or referee_id is null
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 SELECT  name
-FROM    Customer
+FROM    customer
 WHERE   referee_id != 2 or referee_id is null
