649b759613054cb6ccd9b95712548b66ea54f6e9
Rachel Kim <rachelkim2255@gmail.com>
2026-06-23 17:02:19 -0700

    Time: 360 ms (10.56%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,5 +1,46 @@
-# Write your MySQL query statement below
 
-SELECT  name, population, area
-FROM    World
-WHERE   area >= 3000000 OR  population >= 25000000
+
+
+select  name, population, area
+from    world
+where   area >=3000000 or population >=25000000
+
+
+
+
+
+
+
+
+
+
+-- -- world: pk - name
+-- -- condition: area >= 3000000 or population >= 25000000
+
+-- SELECT  name, population, area
+-- FROM    world
+-- WHERE   area >= 3000000 or population >= 25000000
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ # Write your MySQL query statement below
+
+-- SELECT  name, population, area
+-- FROM    World
+-- WHERE   area >= 3000000 OR  population >= 25000000
