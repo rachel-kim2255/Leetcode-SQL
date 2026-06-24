8d2d5aa57408a0ed00808304c71505fa20413c52
Rachel Kim <rachelkim2255@gmail.com>
2026-06-23 17:03:59 -0700

    Time: 334 ms (62.32%), Space: 0B (100%) - LeetHub

--- 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
+++ 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
@@ -1,7 +1,37 @@
-# Write your MySQL query statement below
+-- -- # Write your MySQL query statement below
+
+-- -- class / num of student >= 5
+
+-- SELECT  class
+-- FROM    Courses
+-- GROUP BY class
+-- HAVING  COUNT(*) >= 5
 
 
-SELECT class
-FROM    Courses
+
+
+
+
+
+-- -- SELECT class
+-- -- FROM    Courses
+-- -- group by class
+-- -- HAVING  count(student) >= 5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+select class
+from    courses
 group by class
-HAVING  count(student) >= 5
+having count(*) >4
