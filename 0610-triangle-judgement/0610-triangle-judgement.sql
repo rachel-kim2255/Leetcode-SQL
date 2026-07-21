c0636718d35ba543b2a1575f606f96958dd09be9
Rachel Kim <rachelkim2255@gmail.com>
2026-07-21 12:51:32 -0700

    Time: 348 ms (14.22%), Space: 0B (100%) - LeetHub

--- 0610-triangle-judgement/0610-triangle-judgement.sql
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -1,10 +1,41 @@
-# Write your MySQL query statement below
+-- 7/20 : 두변의 합이 다른 하나보다 작던가
 
-SELECT  x,y,z, 
-        (CASE 
-            WHEN ( x+y > z and y+z > x and x+z > y) THEN 'Yes'
-            ELSE 'No'
-            END)
-            as triangle
-FROM    Triangle
+select  x, y, z, 
+        (CASE WHEN x + y > z and x + z > y and y + z > x THEN 'Yes'
+        ELSE 'No' END) as triangle
+from    triangle
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- SELECT  x,y,z, 
+--         (CASE 
+--             WHEN ( x+y > z and y+z > x and x+z > y) THEN 'Yes'
+--             ELSE 'No'
+--             END)
+--             as triangle
+-- FROM    Triangle
  
+# 그 열만 찾아라( 트라이앵글 열없이)
+-- SELECT  x, y, z
+-- FROM    triangle
+-- WHERE   (x + y > z) and (x+z > y) and (z+y > x)
+
+-- SELECT x, y, z
+-- FROM triangle
+-- WHERE (CASE WHEN x + y > z AND x + z > y AND y + z > x THEN 1 ELSE 0 END) = 1;
