bca0d2f4f7154f472830ad8fcea1b5e3217b68b9
Rachel Kim <rachelkim2255@gmail.com>
2026-07-21 13:07:17 -0700

    Time: 436 ms (61.61%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,11 +1,79 @@
-# KEY: MAX, GROUP BY, IN
-# LEARN: USE WELL IN
+# 7/21 v
 
-SELECT MAX(num) as num
-FROM    MyNumbers
-WHERE num IN (
-    SELECT  num
-    FROM    MyNumbers
-    GROUP BY num
-    HAVING count(*) = 1
- ) 
+
+select max(num) as num
+from (
+select num, count(*)
+from MyNumbers
+group by num
+having count(*) = 1) as sub
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- -- # 이게나음
+-- SELECT max(num) as num
+-- FROM (
+-- SELECT  num
+-- FROM    MyNumbers 
+-- GROUP BY num
+-- HAVING   count(*) = 1) as t
+
+
+-- SELECT MAX(num) as num
+-- FROM (
+-- SELECT  num
+-- FROM    mynumbers
+-- GROUP BY num
+-- HAVING  count(*) = 1)AS T 
+
+
+
+
+
+
+
+
+
+-- -- # KEY: MAX, GROUP BY, IN
+-- -- # LEARN: USE WELL IN
+
+-- -- SELECT MAX(num) as num
+-- -- FROM    MyNumbers
+-- -- WHERE num IN (
+-- --     SELECT  num
+-- --     FROM    MyNumbers
+-- --     GROUP BY num
+-- --     HAVING count(*) = 1
+-- --  ) 
+
+
+
+-- SELECT  num
+-- FROM    mynumbers
+-- GROUP BY num
+-- HAVING case when count(*) = 1 then 1
+--         else 'null' end
+-- ORDER BY   num DESC
+-- LIMIT   1
+
+
+
+
+
+
+
+
