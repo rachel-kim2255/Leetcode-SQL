9a152d2d30f838a1ff8e5f1936137203edcb5d9d
Rachel Kim <rachelkim2255@gmail.com>
2026-07-22 13:26:16 -0700

    Time: 262 ms (44.06%), Space: 0B (100%) - LeetHub

---
+++ 0627-swap-sex-of-employees/0627-swap-sex-of-employees.sql
@@ -0,0 +1,28 @@
+
+
+
+update salary
+set sex = (case when sex = 'm' then 'f'
+            else 'm' end)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- UPDATE Salary
+-- SET  sex = CASE
+--         WHEN sex = 'm' then 'f'
+--         ELSE 'm'
+--         END
+-- -- WHERE salary < 10a00
