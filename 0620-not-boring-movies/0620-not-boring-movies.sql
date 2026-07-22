6c2ed0221e193b0c1c98078ee7a0f75137e9231b
Rachel Kim <rachelkim2255@gmail.com>
2026-07-22 13:18:39 -0700

    Time: 258 ms (81.42%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,8 +1,72 @@
-# Write your MySQL query statement below
+# 7/22 
 
-SELECT  id, movie, description, rating
-FROM    Cinema
-WHERE   CASE WHEN id % 2 = 1 THEN id END
-        AND description != 'boring'
-ORDER BY  rating DESC;
+select  *
+from    cinema
+where   description != 'boring' and id % 2 = 1
+order by rating desc;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- -- -- cinema : id/pk
+-- -- -- odd num id and not boring movie
+-- -- -- order by rating desc
+
+-- -- -- SELECT *
+-- -- -- FROM    cinema
+-- -- -- WHERE   id % 2 = 1 AND description <> 'boring'
+-- -- -- ORDER BY rating desc;
+
+-- -- SELECT *
+-- -- FROM    cinema
+-- -- WHERE   id % 2 = 1 AND description not like 'boring'
+-- -- ORDER BY rating desc;
+
+-- -- -- SELECT  *
+-- -- -- FROM    (SELECT * FROM Cinema WHERE id % 2 = 1) as t
+-- -- -- WHERE   description != '%boring%'
+-- -- -- ORDER BY rating desc;
+
+
+
+
+
+
+
+
+
+-- -- -- # Write your MySQL query statement below
+
+-- -- -- SELECT  id, movie, description, rating
+-- -- -- FROM    Cinema
+-- -- -- WHERE   CASE WHEN id % 2 = 1 THEN id END
+-- -- --         AND description != 'boring'
+-- -- -- ORDER BY  rating DESC;
+
+
+
+
+
+
+-- -- -- cinema : id/pk
+-- -- -- odd num id and not boring movie
+-- -- -- order by rating desc
+
+-- SELECT  id, movie, description, rating
+-- FROM    cinema
+-- WHERE   (id % 2 = 1) and description not like 'boring' 
+-- ORDER BY rating desc;
+
+
 
