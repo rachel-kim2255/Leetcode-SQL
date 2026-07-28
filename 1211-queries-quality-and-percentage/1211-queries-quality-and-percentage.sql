53f47296dbd9b3d9d012ec076a3ac612127c1267
Rachel Kim <rachelkim2255@gmail.com>
2026-07-27 23:01:12 -0700

    Time: 952 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -2,7 +2,7 @@
 
 select  query_name, 
         round( avg(rating/position) , 2) as quality,
-        round(((sum(case when rating < 3 then 1 else 0 end) / count(rating)) * 100),2) as poor_query_percentage
+        round((sum(case when rating < 3 then 1 else 0 end) / count(rating) * 100),2) as poor_query_percentage
 from    queries
 group by query_name
 
