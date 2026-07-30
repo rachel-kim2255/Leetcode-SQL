2061cac6e7458cc3c2d303c033a373fdd6e75dd6
Rachel Kim <rachelkim2255@gmail.com>
2026-07-29 21:27:28 -0700

    Time: 504 ms (31.41%), Space: 0B (100%) - LeetHub

--- 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
+++ 1484-group-sold-products-by-the-date/1484-group-sold-products-by-the-date.sql
@@ -1,6 +1,35 @@
-# LEARN: GROUP_CONCAT(name SEPARATOR '')
-SELECT  sell_date, count(distinct product) as num_sold,
-        GROUP_CONCAT(DISTINCT product order by product ASC SEPARATOR ',') AS products
-FROM    Activities
-GROUP BY   sell_date
-ORDER BY   sell_date asc; 
+# 7/29
+
+select  sell_date, count(distinct product) as num_sold,
+        group_concat(distinct product separator ',') as products
+from    activities
+group by sell_date
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- # LEARN: GROUP_CONCAT(name SEPARATOR '')
+-- SELECT  sell_date, count(distinct product) as num_sold,
+--         GROUP_CONCAT(DISTINCT product order by product ASC SEPARATOR ',') AS products
+-- FROM    Activities
+-- GROUP BY   sell_date
+-- ORDER BY   sell_date asc; 
