ca92959288b4e164609aaa68dc063709e4459299
Rachel Kim <rachelkim2255@gmail.com>
2026-07-31 14:36:13 -0700

    Time: 752 ms (76.3%), Space: 0B (100%) - LeetHub

--- 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
+++ 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
@@ -1,6 +1,30 @@
-# learn: case-sensitive - 'c'
+# 7/31
 
-SELECT *
-FROM Users
-WHERE REGEXP_LIKE(mail, '^[a-zA-Z]+[A-Za-z0-9._-]*@leetcode[.]com$','c');
+select  *
+from    users
+where   REGEXP_LIKE(mail, '^[a-zA-Z]+[A-Za-z0-9._-]*@leetcode[.]com$','c')
+        # REGEXP_LIKE(mail, '^[a-zA-Z]+[a-zA-Z0-9._-]*@leetcode[.]com$') -> contain 'leetcode.COM' (X) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- # learn: case-sensitive - 'c'
+
+-- SELECT *
+-- FROM Users
+-- WHERE REGEXP_LIKE(mail, '^[a-zA-Z]+[A-Za-z0-9._-]*@leetcode[.]com$','c');
 
