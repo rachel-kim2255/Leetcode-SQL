346cfa8af2a16bb9ad2da6280772201b96894e55
Rachel Kim <rachelkim2255@gmail.com>
2026-08-01 16:31:39 -0700

    Time: 1325 ms (79.66%), Space: 0B (100%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,8 +1,16 @@
 # 8/1 
 
-select  v.customer_id, 
-        count(v.visit_id) - count(t.transaction_id) as count_no_trans
-from    visits as v LEFT JOIN transactions as t
-        on v.visit_id = t.visit_id
-group by customer_id
-having count(v.visit_id) - count(t.transaction_id) <> 0
+-- select  v.customer_id, 
+--         count(v.visit_id) - count(t.transaction_id) as count_no_trans
+-- from    visits as v LEFT JOIN transactions as t
+--         on v.visit_id = t.visit_id
+-- group by customer_id
+-- having count(v.visit_id) - count(t.transaction_id) <> 0
+
+
+
+SELECT  v.customer_id, count(*) as count_no_trans
+FROM    Visits v LEFT JOIN Transactions t
+        ON v.visit_id = t.visit_id
+WHERE   t.transaction_id is null
+GROUP BY v.customer_id
