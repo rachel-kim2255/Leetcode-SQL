a24b1955ff8b3737ae6e87a2bed916fd32bb6084
Rachel Kim <rachelkim2255@gmail.com>
2026-08-01 15:55:45 -0700

    Time: 400 ms (82.57%), Space: 0B (100%) - LeetHub

--- 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -1,5 +1,32 @@
-# Write your MySQL query statement below
+# 7.31 다시
 
-SELECT  *
-FROM    Patients
-WHERE   conditions LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%'
+-- select  *
+-- from    patients
+-- where   conditions like 'DIAB1%' OR conditions like '% DIAB1%'
+
+
+
+SELECT *
+FROM Patients
+WHERE REGEXP_LIKE(conditions, '(^| )DIAB1')
+
+
+
+
+
+
+
+
+
+
+
+
+-- # Write your MySQL query statement below
+
+-- SELECT  *
+-- FROM    Patients
+-- WHERE   conditions LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%'
+
+-- SELECT *
+-- FROM Patients
+-- WHERE REGEXP_LIKE(conditions, '(^| )DIAB1') > start with the letters or blank, and with the letters 
