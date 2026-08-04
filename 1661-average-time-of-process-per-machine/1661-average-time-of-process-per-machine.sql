f910269ad3e9568e523ac937cd082190e9463268
Rachel Kim <rachelkim2255@gmail.com>
2026-08-04 16:50:10 -0700

    Time: 247 ms (80.24%), Space: 0B (100%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,13 +1,6 @@
 # 8/4 
 
--- -- SELECT 
--- --     machine_id,
--- --     ROUND(SUM(
--- --           CASE WHEN activity_type = 'end' THEN timestamp ELSE -timestamp END) 
---             / COUNT(DISTINCT process_id), 3
--- --     ) AS processing_time
--- -- FROM Activity
--- -- GROUP BY machine_id;
+
 
 SELECT  machine_id,
         round(SUM(CASE WHEN activity_type = 'end' then timestamp else - timestamp end) / 
