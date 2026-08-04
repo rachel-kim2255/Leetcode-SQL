0d9387a43283ff6b2d1fcd8491e0e83b2772170d
Rachel Kim <rachelkim2255@gmail.com>
2026-08-04 16:50:08 -0700

    Time: 261 ms (55.09%), Space: 0B (100%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,12 +1,47 @@
-# activity: pk(machine_id, process_id, activity_type)
+# 8/4 
+
+-- -- SELECT 
+-- --     machine_id,
+-- --     ROUND(SUM(
+-- --           CASE WHEN activity_type = 'end' THEN timestamp ELSE -timestamp END) 
+--             / COUNT(DISTINCT process_id), 3
+-- --     ) AS processing_time
+-- -- FROM Activity
+-- -- GROUP BY machine_id;
+
+SELECT  machine_id,
+        round(SUM(CASE WHEN activity_type = 'end' then timestamp else - timestamp end) / 
+        COUNT(DISTINCT process_id), 3)
+        as processing_time
+FROM    Activity
+GROUP BY machine_id
 
 
 
-SELECT  a1.machine_id,
-        round(AVG(a2.timestamp - a1.timestamp), 3) as processing_time
-FROM    activity a1 JOIN activity a2
-        ON a1.machine_id = a2.machine_id 
-        AND a1.process_id = a2.process_id
-        AND a2.activity_type = 'end'
-        AND a1.activity_type = 'start'
-GROUP BY a1.machine_id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- # activity: pk(machine_id, process_id, activity_type)
+
+
+
+-- SELECT  a1.machine_id,
+--         round(AVG(a2.timestamp - a1.timestamp), 3) as processing_time
+-- FROM    activity a1 JOIN activity a2
+--         ON a1.machine_id = a2.machine_id 
+--         AND a1.process_id = a2.process_id
+--         AND a2.activity_type = 'end'
+--         AND a1.activity_type = 'start'
+-- GROUP BY a1.machine_id;
+
