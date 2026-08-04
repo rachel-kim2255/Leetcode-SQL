b16690ed681180d50bc0c7a712ee9db1a1a76d96
Rachel Kim <rachelkim2255@gmail.com>
2026-08-04 16:59:07 -0700

    Time: 1144 ms (5%), Space: 0B (100%) - LeetHub

--- 1683-invalid-tweets/1683-invalid-tweets.sql
+++ 1683-invalid-tweets/1683-invalid-tweets.sql
@@ -1,5 +1,25 @@
-# Tweets: pk(tweet_id)
 
-SELECT  tweet_id
-FROM    Tweets
-WHERE  LENGTH(content) > 15
+select  tweet_id
+from    tweets
+where   length(content) > 15
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- # Tweets: pk(tweet_id)
+
+-- SELECT  tweet_id
+-- FROM    Tweets
+-- WHERE  LENGTH(content) > 15
