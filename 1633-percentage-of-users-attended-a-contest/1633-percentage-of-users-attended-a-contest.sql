c05ccbc2b7964b8f10b9420c9381f8ac2cc42507
Rachel Kim <rachelkim2255@gmail.com>
2026-08-04 16:12:21 -0700

    Time: 1031 ms (51.87%), Space: 0B (100%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -1,10 +1,38 @@
-# users: user_id(pk)
-# register: pk(contest_id, user_id)
+# 8/4
 
 
-SELECT  r.contest_id as contest_id,
-        round((COUNT(r.user_id) * 100.0)/ (SELECT count(user_id) FROM users),2) as percentage
-FROM    register r
-GROUP BY r.contest_id
-ORDER BY percentage desc, contest_id asc;
+select  contest_id, 
+        round(count(user_id) / (select count(*) from users) * 100, 2) as percentage
+from    register
+group by contest_id
+order by percentage desc, contest_id asc;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- # users: user_id(pk)
+-- # register: pk(contest_id, user_id)
+
+
+-- SELECT  r.contest_id as contest_id,
+--         round((COUNT(r.user_id) * 100.0)/ (SELECT count(user_id) FROM users),2) as percentage
+-- FROM    register r
+-- GROUP BY r.contest_id
+-- ORDER BY percentage desc, contest_id asc;
 
