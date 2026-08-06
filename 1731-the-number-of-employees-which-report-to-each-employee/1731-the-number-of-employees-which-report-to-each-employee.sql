4d1bd6735a8ad3863972e69db6f29c768b4dd670
Rachel Kim <rachelkim2255@gmail.com>
2026-08-05 22:04:32 -0700

    Time: 726 ms (34.38%), Space: 0B (100%) - LeetHub

--- 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -1,32 +1,5 @@
 # 8.5
 
-with list as(
-select  reports_to,count(reports_to) as counted, round(avg(age),0) as average_age
-from    employees
-group by reports_to)
-select  e.employee_id, e.name, l.counted as reports_count, l.average_age
-from    employees as e LEFT JOIN list as l
-        on e.employee_id = l.reports_to
-where   l.reports_to is not null
-order by e.employee_id
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 -- SELECT  e1.employee_id, e1.name, 
@@ -36,3 +9,39 @@ order by e.employee_id
 --         ON e1.employee_id = e2.reports_to
 -- GROUP BY e1.employee_id, e1.name
 -- ORDER BY e1.employee_id;
+
+
+SELECT  e1.employee_id, e1.name, count(e2.employee_id) as reports_count, round(avg(e2.age),0) as average_age
+FROM    employees as e1 inner join employees as e2
+        on e1.employee_id = e2.reports_to
+group by e1.employee_id
+order by e1.employee_id;
+-- with list as(
+-- select  reports_to,count(reports_to) as counted, round(avg(age),0) as average_age
+-- from    employees
+-- group by reports_to)
+-- select  e.employee_id, e.name, l.counted as reports_count, l.average_age
+-- from    employees as e LEFT JOIN list as l
+--         on e.employee_id = l.reports_to
+-- where   l.reports_to is not null
+-- order by e.employee_id
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
