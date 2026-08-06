a2faed74e7808c27f1da920f52e565fded6c9dd7
Rachel Kim <rachelkim2255@gmail.com>
2026-08-05 21:44:10 -0700

    Time: 620 ms (34.59%), Space: 0B (100%) - LeetHub

--- 1729-find-followers-count/1729-find-followers-count.sql
+++ 1729-find-followers-count/1729-find-followers-count.sql
@@ -1,5 +1,29 @@
 
 
-SELECT user_id, count(distinct follower_id) as followers_count
-FROM    followers
-GROUP BY user_id
+
+
+
+
+# 8/5 v
+
+select  user_id, count(*) as followers_count
+from    followers
+group by user_id
+order by user_id asc;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- SELECT user_id, count(distinct follower_id) as followers_count
+-- FROM    followers
+-- GROUP BY user_id
