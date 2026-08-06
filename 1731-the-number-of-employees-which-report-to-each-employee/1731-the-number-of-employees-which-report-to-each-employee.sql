34cd5269a63abc2e61717a58286ca42a29c0f43c
Rachel Kim <rachelkim2255@gmail.com>
2026-08-05 22:01:08 -0700

    Time: 883 ms (11.58%), Space: 0B (100%) - LeetHub

--- 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -1,9 +1,38 @@
+# 8.5
+
+with list as(
+select  reports_to,count(reports_to) as counted, round(avg(age),0) as average_age
+from    employees
+group by reports_to)
+select  e.employee_id, e.name, l.counted as reports_count, l.average_age
+from    employees as e LEFT JOIN list as l
+        on e.employee_id = l.reports_to
+where   l.reports_to is not null
+order by e.employee_id
 
 
-SELECT  e1.employee_id, e1.name, 
-        COUNT(e2.reports_to) AS reports_count, 
-        ROUND(AVG(e2.age), 0) AS average_age
-FROM    employees as e1 INNER JOIN employees as e2
-        ON e1.employee_id = e2.reports_to
-GROUP BY e1.employee_id, e1.name
-ORDER BY e1.employee_id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- SELECT  e1.employee_id, e1.name, 
+--         COUNT(e2.reports_to) AS reports_count, 
+--         ROUND(AVG(e2.age), 0) AS average_age
+-- FROM    employees as e1 INNER JOIN employees as e2
+--         ON e1.employee_id = e2.reports_to
+-- GROUP BY e1.employee_id, e1.name
+-- ORDER BY e1.employee_id;
