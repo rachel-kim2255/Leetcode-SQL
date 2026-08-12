809de6343b2220c4c66c4cbb92f5cfa24ad5ff9b
Rachel Kim <rachelkim2255@gmail.com>
2026-08-12 13:42:05 -0700

    Time: 543 ms (69.65%), Space: 0B (100%) - LeetHub

--- 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
@@ -1,5 +1,27 @@
-
-
+# 8.12 V 
 SELECT  product_id
-FROM    products
-WHERE   low_fats='Y' AND recyclable='Y'
+FROM    Products
+where   low_fats = 'Y' and recyclable = 'Y'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- SELECT  product_id
+-- FROM    products
+-- WHERE   low_fats='Y' AND recyclable='Y'
