a1b3d253b0bd542f9d0cd30b14bf2d81a3b0dd03
Rachel Kim <rachelkim2255@gmail.com>
2026-08-12 13:39:42 -0700

    Time: 604 ms (35.51%), Space: 0B (100%) - LeetHub

--- 1741-find-total-time-spent-by-each-employee/1741-find-total-time-spent-by-each-employee.sql
+++ 1741-find-total-time-spent-by-each-employee/1741-find-total-time-spent-by-each-employee.sql
@@ -1,6 +1,34 @@
+# 8/12
+
+select  event_day as day, emp_id,
+        sum(out_time - in_time) as total_time
+from    Employees
+group by day, emp_id
 
 
-SELECT  event_day as day, emp_id,
-        SUM(out_time-in_time) AS total_time
-FROM    employees
-GROUP BY event_day, emp_id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- SELECT  event_day as day, emp_id,
+--         SUM(out_time-in_time) AS total_time
+-- FROM    employees
+-- GROUP BY event_day, emp_id;
