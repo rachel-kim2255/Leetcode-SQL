b2466d0f1fec0fc1aa23391708409e1eca8ca4dc
Rachel Kim <rachelkim2255@gmail.com>
2026-09-15 21:45:40 -0700

    Time: 648 ms (80.5%), Space: 0B (100%) - LeetHub

--- 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -1,11 +1,5 @@
 # 9.15
 
--- select  user_id,
---         (case when year(time_stamp) = 2020 then max(time_stamp))
---         as last_stamp
--- from    logins
--- group by user_id
-
 
 select user_id,
         max(time_stamp) as last_stamp
