87ca067ed3ae275e8279c45d67c97637b1bc32ef
Rachel Kim <rachelkim2255@gmail.com>
2026-09-15 21:14:49 -0700

    Time: 526 ms (94.01%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,9 +1,50 @@
+-- # 9/15 again
 
-(SELECT  employee_id,department_id
-FROM    employee
-GROUP BY employee_id
-HAVING  COUNT(*) = 1)
+(select  employee_id, department_id
+from    employee
+where   primary_flag = 'Y')
 union
-(SELECT employee_id, department_id
-FROM    employee
-WHERE   primary_flag = 'Y')
+(select employee_id, department_id
+from    employee
+group by employee_id
+having count(*) = 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- -- (SELECT  employee_id,department_id
+-- -- FROM    employee
+-- -- GROUP BY employee_id
+-- -- HAVING  COUNT(*) = 1) -- someone who has only one department (whether primary or not)
+-- -- union
+-- -- (SELECT employee_id, department_id
+-- -- FROM    employee
+-- -- WHERE   primary_flag = 'Y')
+
+
+-- (select  employee_id, department_id
+-- from    employee
+-- group by employee_id
+-- having count(*) = 1)
+-- union
+-- (select employee_id, department_id
+-- from    employee
+-- where   primary_flag = 'Y')
